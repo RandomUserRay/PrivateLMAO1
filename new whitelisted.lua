a67cb1ac2dc49a71a25758d5aacefffcb85e1cb1
RandomUserRay <145318415+RandomUserRay@users.noreply.github.com>
2023-10-23 14:22:46 +0700

    Add files via upload

---
+++ new whitelisted.lua
@@ -0,0 +1,74 @@
+
+
+
+{
+  "RayHafz": [
+    {"id": "4984819342"},
+	{"id": "1"}
+  ],
+  "Homies": [
+    {"id": "5049082159"},
+    {"id": "5050497212"}, 
+    {"id": "5083508263"}
+  ],
+  "Owner": [
+    {"id": "4853841349"},
+    {"id": "241800198"},
+    {"id": "3680034704"},
+    {"id": "5056406707"},
+    {"id": "2"}
+  ],
+  "CoOwner": [
+    {"id": "5049603219"},
+    {"id": "3"},
+    {"id": "4971126290"},
+    {"id": "5027789239"},
+    {"id": "4791243291"},
+    {"id": "4950605144"},
+    {"id": "4670661281"}
+  ],
+  "PrivatePlus": [
+    {"id": "4967163214"},
+    {"id": "5000289307"},
+    {"id": "241800198"},
+    {"id": "5057797886"},
+    {"id": "5096573047"},
+    {"id": "6"},
+    {"id": "6"},
+    {"id": "6"}
+  ],
+  "Private": [
+    {"id": "5045660230"},
+    {"id": "5025491014"},
+    {"id": "4903117035"},
+    {"id": "5047134633"},
+    {"id": "5048777066"},
+    {"id": "5030045060"},
+    {"id": "11"},
+    {"id": "11"},
+    {"id": "11"},
+    {"id": "11"},
+    {"id": "11"},
+    {"id": "11"},
+    {"id": "11"},
+    {"id": "11"}
+  ],
+  "tags": [
+    {"userid": "5043682545", "tag": "RayHafz BF", "color": "blue"},
+    {"userid": "5071878111", "tag": "VAPE OWNER", "color": "red"},
+    {"userid": "5049082159", "tag": "VAPE DEVELOPER", "color": "red"},
+    {"userid": "5049603219", "tag": "Zed BF", "color": "purple"},
+    {"userid": "5063568907", "tag": "VAPE PRIVATE", "color": "purple"},
+    {"userid": "5083508263", "tag": "MW OWNER", "color": "purple"},
+    {"userid": "1", "tag": "nn", "color": "yellow"},
+    {"userid": "1", "tag": "nn", "color": "blue"},
+    {"userid": "1", "tag": "nn", "color": "purple"},
+    {"userid": "1", "tag": "nn", "color": "yellow"},
+    {"userid": "1", "tag": "nn", "color": "cyan"},
+    {"userid": "1", "tag": "nn", "color": "purple"},
+    {"userid": "1", "tag": "nn", "color": "pink"},
+    {"userid": "1", "tag": "nn", "color": "purple"},
+    {"userid": "1", "tag": "nn", "color": "purple"},
+    {"userid": "1", "tag": "nn", "color": "red"}
+  ]
+}
